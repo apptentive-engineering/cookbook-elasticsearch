a1be10b533b853ca5582d2abbf63585a2308b1c3
Andres Rojas <andres@conpat.io>
2018-12-26 17:32:34 -0500

    Add support for v6.5.4

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ maintainer_email 'karel.minarik@elasticsearch.org'
 license          'Apache 2.0'
 description      'Installs and configures Elasticsearch'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '4.0.4'
+version          '4.0.5'
 
 supports 'amazon'
 supports 'centos'
@@ -19,7 +19,7 @@ depends 'yum'
 depends 'chef-sugar'
 depends 'ark'
 
-issues_url       'https://github.com/elastic/cookbook-elasticsearch/issues'
-source_url       'https://github.com/elastic/cookbook-elasticsearch'
+issues_url       'https://github.com/elastic/cookbook-elasticsearch/issues' if respond_to?(:issues_url)
+source_url       'https://github.com/elastic/cookbook-elasticsearch' if respond_to?(:source_url)
 
 chef_version '>= 12.5' if respond_to?(:chef_version)
